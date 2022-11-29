be067ec844fdb5136315bce14308edef3ae0c779
yanJuicy <hanjub3@gmail.com>
2022-11-29 16:50:02 +0900

    initial commit

---
+++ src/main/java/com/hanghae/blog/hanghaeblog/HanghaeBlogApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.blog.hanghaeblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaeBlogApplication.class, args);
+    }
+
+}
