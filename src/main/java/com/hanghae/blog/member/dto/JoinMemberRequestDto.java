9a932755aa30b28ea0a88d46f059139c92e24205
yanJuicy <hanjub3@gmail.com>
2022-12-06 17:57:28 +0900

    Login Dto validation 제거

--- src/main/java/com/hanghae/blog/member/dto/JoinMemberRequestDto.java
+++ src/main/java/com/hanghae/blog/member/dto/JoinMemberRequestDto.java
@@ -3,8 +3,6 @@ package com.hanghae.blog.member.dto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import javax.validation.constraints.Size;
-
 @Getter
 @RequiredArgsConstructor
 public class JoinMemberRequestDto {
