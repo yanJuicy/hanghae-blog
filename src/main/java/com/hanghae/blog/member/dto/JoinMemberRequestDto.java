b8d4122280f0d80c360153e50dc9eab2be5de6ce
yanJuicy <hanjub3@gmail.com>
2022-12-07 11:43:03 +0900

    정규식 상수화

--- src/main/java/com/hanghae/blog/member/dto/JoinMemberRequestDto.java
+++ src/main/java/com/hanghae/blog/member/dto/JoinMemberRequestDto.java
@@ -9,13 +9,15 @@ import javax.validation.constraints.Size;
 @Getter
 @RequiredArgsConstructor
 public class JoinMemberRequestDto {
+    private static final String USERNAME_PATTERN = "^[a-z0-9]+[a-z]+[0-9]+";
+    private static final String PASSWORD_PATTERN = "^[A-Za-z0-9\\{\\}\\[\\]\\/?.,;:|\\)*~`!^\\-_+<>@\\#$%&\\\\\\=\\(\\'\\\"]+";
 
     @Size(min = 4, max = 10)
-    @Pattern(regexp = "^[a-z0-9]+[a-z]+[0-9]+")
+    @Pattern(regexp = USERNAME_PATTERN)
     private final String username;
 
     @Size(min = 8, max = 15)
-    @Pattern(regexp = "^[A-Za-z0-9\\{\\}\\[\\]\\/?.,;:|\\)*~`!^\\-_+<>@\\#$%&\\\\\\=\\(\\'\\\"]+")
+    @Pattern(regexp = PASSWORD_PATTERN)
     private final String password;
 
 }
