01bb505bd2f1296c9504dc79bde98eda504439b4
yanJuicy <hanjub3@gmail.com>
2022-12-07 11:40:57 +0900

    비밀번호 특수문자 포함

--- src/main/java/com/hanghae/blog/member/dto/JoinMemberRequestDto.java
+++ src/main/java/com/hanghae/blog/member/dto/JoinMemberRequestDto.java
@@ -3,11 +3,19 @@ package com.hanghae.blog.member.dto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Getter
 @RequiredArgsConstructor
 public class JoinMemberRequestDto {
 
+    @Size(min = 4, max = 10)
+    @Pattern(regexp = "^[a-z0-9]+[a-z]+[0-9]+")
     private final String username;
+
+    @Size(min = 8, max = 15)
+    @Pattern(regexp = "^[A-Za-z0-9\\{\\}\\[\\]\\/?.,;:|\\)*~`!^\\-_+<>@\\#$%&\\\\\\=\\(\\'\\\"]+")
     private final String password;
 
 }
--- src/main/java/com/hanghae/blog/member/dto/LoginMemberRequestDto.java
+++ src/main/java/com/hanghae/blog/member/dto/LoginMemberRequestDto.java
@@ -3,18 +3,10 @@ package com.hanghae.blog.member.dto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
-
 @Getter
 @RequiredArgsConstructor
 public class LoginMemberRequestDto {
 
-    @Size(min = 4, max = 10)
-    @Pattern(regexp = "^[a-z0-9]+")
     private final String username;
-
-    @Size(min = 8, max = 15)
-    @Pattern(regexp = "^[A-Za-z0-9]+")
     private final String password;
 }
